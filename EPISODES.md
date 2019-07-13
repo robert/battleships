c9cb6a84c6ee7ab630bbad739835baf5fde71976
Robert Heaton <bobjheaton@gmail.com>
2019-07-12 18:23:07 -0700

    Episode 1 - get input and render board

--- EPISODES.md
+++ EPISODES.md
@@ -2,3 +2,11 @@
 
 [x] Initialize git
 [x] Setup main.py
+
+# EPISODE 2
+
+[x] Get input from player
+[x] Parse into x and y co-ordinates
+
+[x] Render an empty board
+[] Render a board with shots
--- main.py
+++ main.py
@@ -1,4 +1,32 @@
 
 
+def render(board_width, board_height, shots):
+    header = "+" + "-" * board_width + "+"
+    print(header)
+
+    shots_set = set(shots)
+    for y in range(board_height):
+        row = []
+        for x in range(board_width):
+            if (x,y) in shots_set:
+                ch = "X"
+            else:
+                ch = " "
+            row.append(ch)
+        print("|" + "".join(row) + "|")
+
+    print(header)
+
 if __name__ == "__main__":
-    print("HELLO")
+    shots = []
+
+    while True:
+        inp = input("Where do you want to shoot?\n")
+        # TODO: deal with invalid input
+        xstr, ystr = inp.split(",")
+        x = int(xstr)
+        y = int(ystr)
+
+        shots.append((x,y))
+        render(10, 10, shots)
+
